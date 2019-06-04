065e2c8f40dbeded439bdba2f743640671fd135d
Marc Rhyo K. Lomboy <mlomboy@SVI.COM.PH>
2019-06-04 19:58:57 +0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonSearchComponent } from './pokemon-search/pokemon-search.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonSearchComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
