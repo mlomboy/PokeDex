e47a6c4b21bde94cabd45cf50ec38a1bff49ad59
Marc Rhyo K. Lomboy <mlomboy@SVI.COM.PH>
2019-06-10 22:04:33 +0800

    Pokemon Object and Services

--- src/app/pokemon-details.ts
+++
@@ -1,6 +0,0 @@
-export interface PokemonDetails {
-    pokeName : string;
-    pokeId : number;
-    pokeDesc : string;
-    pokeType : string;
-}
--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {PokemonDetails} from './Pokemondetails';
+import { catchError, map, tap } from 'rxjs/operators';
+import { MessageService } from './message.service';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService ) { }
+
+  private pokeUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  getHeroes(): Observable<PokemonDetails[]> {
+    return this.http.get<PokemonDetails[]>(this.pokeUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<PokemonDetails[]>('getPokemon', []))
+      );
+
+  }
+      private handleError<T>(operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
+    
+          // TODO: send the error to remote logging infrastructure
+          console.error(error); // log to console instead
+    
+          // TODO: better job of transforming error for user consumption
+          this.log(`${operation} failed: ${error.message}`);
+    
+          // Let the app keep running by returning an empty result.
+          return of(result as T);
+        };
+      }
+      private log(message: string) {
+        this.messageService.add(`HeroService: ${message}`);
+      }
 }
+
